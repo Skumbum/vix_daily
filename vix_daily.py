d100d50a6bea8a6a4bf0d3868a92dfd35801e4ae
Skumbum <170135869+Skumbum@users.noreply.github.com>
2025-04-02 19:14:24 +0100

    Add vix_daily.py and .idea/misc.xml

--- vix_daily.py
+++ vix_daily.py
@@ -44,5 +44,3 @@ print(f"Median: {median_vix:.2f}")
 print(f"Mode: {mode_vix:.2f} (occurs {mode_count} times)")
 print(f"Std Dev: {std_dev_vix:.2f}")
 print(f"Z-Score: {z_score:.2f}")
-
-print("bye")
